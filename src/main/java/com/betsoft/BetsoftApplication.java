a4522435f1b39a4ee1468af89b97666a210b35c1
MCKM <macura.kamil@gmail.com>
2017-10-12 21:28:24 +0200

    init commit

---
+++ src/main/java/com/betsoft/BetsoftApplication.java
@@ -0,0 +1,12 @@
+package com.betsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetsoftApplication.class, args);
+	}
+}
---
+++ src/main/java/com/betsoft/model/security/AuthorityName.java
@@ -0,0 +1,8 @@
+package com.betsoft.model.security;
+
+/**
+ * Created by MCKM on 12.10.2017.
+ */
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
